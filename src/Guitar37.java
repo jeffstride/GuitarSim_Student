2cc76133b1d01b783a86caa05ce7ee9965038ffa
Jeff Z Stride <jeffstride@hotmail.com>
2020-09-14 16:41:24 -0700

    Create Guitar37.java

---
+++ src/Guitar37.java
@@ -0,0 +1,44 @@
+//skeleton version of the class
+
+public class Guitar37 implements PlayableGuitar {
+	public static final String KEYBOARD =
+			"q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";  // keyboard layout
+
+	@Override
+	public void playNote(int pitch) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean hasString(char key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void pluck(char key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double sample() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void tic() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int time() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+}
